ebbcbe3809667d3ab0c03efce73f67ff1fa742a6
Prayansh Srivastava <prayansh@gmail.com>
2022-05-11 11:00:16 -0700

    add extension apis for interacting with JSValue

--- substrata-kotlin/src/main/java/com/segment/analytics/substrata/kotlin/j2v8/RuntimeDataExtensions.kt
+++ substrata-kotlin/src/main/java/com/segment/analytics/substrata/kotlin/j2v8/RuntimeDataExtensions.kt
@@ -2,7 +2,9 @@ package com.segment.analytics.substrata.kotlin.j2v8
 
 import com.segment.analytics.kotlin.core.BaseEvent
 import com.segment.analytics.substrata.kotlin.JSValue
+import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
 
 fun J2V8Engine.toJSObject(event: BaseEvent): JSValue.JSObject =
     JSValue.JSObject(event.toV8Object(underlying))
@@ -12,3 +14,12 @@ fun J2V8Engine.toJSObject(map: Map<String, JsonElement>): JSValue.JSObject =
 
 fun J2V8Engine.toJSArray(list: List<JsonElement>): JSValue.JSArray =
     JSValue.JSArray(underlying.toV8Array(list))
+
+fun <T: BaseEvent> toSegmentEvent(jsObj: JSValue.JSObject): T? =
+    jsObj.content.toSegmentEvent()
+
+fun fromJSObject(jsObj: JSValue.JSObject): JsonObject? =
+    fromV8Object(jsObj.content)
+
+fun fromJSArray(jsObj: JSValue.JSArray): JsonArray? =
+    fromV8Array(jsObj.content)
