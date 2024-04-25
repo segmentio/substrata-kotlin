5a2467a589114bbd2bf8710bb656e735e0549fba
Wenxi Zeng <wzeng@twilio.com>
2024-04-25 13:19:07 -0500

    add comments

--- substrata-kotlin/src/main/java/com/segment/analytics/substrata/kotlin/SafeEngine.kt
+++ substrata-kotlin/src/main/java/com/segment/analytics/substrata/kotlin/SafeEngine.kt
@@ -20,6 +20,16 @@ class JSScope(
         }.get()
     }
 
+    /**
+     * run a task in background.
+     *
+     * if `global` set to true, the MemoryManager won't automatically release the JSValues created
+     * within the task, making the values persist across tasks. values persisted in the global scope
+     * are released when the engine is released. do not abuse global scope to avoid OOM exception
+     *
+     * @param global whether to run the task in the global scope.
+     * @param closure content of the task
+     */
     inline fun launch(global: Boolean = false, crossinline closure: JSEngine.() -> Unit) {
         try {
             executor.submit {
@@ -37,6 +47,16 @@ class JSScope(
         }
     }
 
+    /**
+     * run a task in a blocking way.
+     *
+     * if `global` set to true, the MemoryManager won't automatically release the JSValues created
+     * within the task, making the values persist across tasks. values persisted in the global scope
+     * are released when the engine is released. do not abuse global scope to avoid OOM exception
+     *
+     * @param global whether to run the task in the global scope.
+     * @param closure content of the task
+     */
     inline fun sync(global: Boolean = false, crossinline closure: JSEngine.() -> Unit) {
         try {
             executor.submit {
@@ -54,6 +74,20 @@ class JSScope(
         }
     }
 
+    /**
+     * run a task in a blocking way and return the value of the task.
+     *
+     * if `global` set to true, the MemoryManager won't automatically release the JSValues created
+     * within the task, making the values persist across tasks. values persisted in the global scope
+     * are released when the engine is released. do not abuse global scope to avoid OOM exception.
+     *
+     * NOTE: do not return JSValue from the closure/task, since the returned JSValue might be already
+     * released when exiting the scope. convert JSValue to the type you want and return the converted
+     * value.
+     *
+     * @param global whether to run the task in the global scope.
+     * @param closure content of the task
+     */
     inline fun <T> await(global: Boolean = false, crossinline closure: JSEngine.() -> T): T? {
         return try {
             executor.submit(Callable {
